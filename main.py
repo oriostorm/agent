8dd63f9f78e875ca83f011db03cb7dafd894e0f7
Orion Storm <orionstorm95@gmail.com>
2025-06-27 19:32:02 -0400

    adding argv implementation

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 def main():
+
+    num_args = len(sys.argv)
+
+    if num_args == 1 or num_args > 2:
+        print("Usage: python3 main.py <prompt for Gemini to answer>")
+        sys.exit(1)
+
+    gemini_request_and_response(sys.argv[1])
+
+
+def gemini_request_and_response(prompt):
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    print(api_key)
+    #print(api_key)
 
     model = "gemini-2.0-flash-001"
  #   call_content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=prompt
     )
     print(response.text)
 
